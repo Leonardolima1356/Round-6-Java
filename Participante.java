e8fc771ea62b299b2509df953a25fe94a155079c
Leonardo Lima <leonardolima1356@gmail.com>
2025-06-15 18:50:34 -0300

    Update Participante.java

--- Participante.java
+++ Participante.java
@@ -2,11 +2,17 @@ public class Participante {
     private String nome;
     private int numero;
     private int idade;
+    private String dataNascimento;
+    private String genero;
+    private String endereco;
 
-    public Participante(String nome, int numero, int idade) {
+    public Participante(String nome, int numero, int idade, String dataNascimento, String genero, String endereco) {
         this.nome = nome;
         this.numero = numero;
         this.idade = idade > 0 ? idade : 0;
+        this.dataNascimento = dataNascimento;
+        setGenero(genero);
+        this.endereco = endereco;
     }
 
     public String getNome() {
@@ -19,6 +25,18 @@ public class Participante {
 
     public int getIdade() {
         return idade;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getEndereco() {
+        return endereco;
     }
 
     public void setNome(String nome) {
@@ -35,8 +53,29 @@ public class Participante {
         }
     }
 
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public void setGenero(String genero) {
+        if (genero.equalsIgnoreCase("Masculino") || genero.equalsIgnoreCase("Feminino")) {
+            this.genero = genero;
+        } else {
+            this.genero = "Não especificado";
+        }
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
     @Override
     public String toString() {
-        return "\nNome: " + nome + "\nNúmero: " + numero + "\nIdade: \n" + idade;
+        return "\nNome: " + nome +
+               "\nNúmero: " + numero +
+               "\nIdade: " + idade +
+               "\nData de Nascimento: " + dataNascimento +
+               "\nGênero: " + genero +
+               "\nEndereço: " + endereco;
     }
 }
